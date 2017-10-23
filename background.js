54426cefbc8cdcab60a05b0168740741ead43098
eight <eight04@gmail.com>
2017-10-24 00:01:00 +0800

    Refactor

--- background.js
+++ background.js
@@ -1,164 +1,228 @@
-/* eslint-env webextensions, browser */
-var {windows, contextMenus, tabs} = browser;
+/* eslint-env webextensions */
 
-var windowList = new Map;
+const windows = (function () {
+	const windowList = new Map;
+	let handleFocusChange;
+	let lastFocusedNormalWindow;
+	
+	const initialize = browser.windows
+		.getAll({windowTypes: ["normal", "popup"]})
+		.then(ws => {
+			ws.forEach(addWindow);
+			return ws.find(w => w.focused);
+		});
 
-var MERGE_CONTEXT, POPUP_CONTEXT, TAB_CONTEXT;
-
-try {
-	// tab context menu only works on Firefox 53+
-	TAB_CONTEXT = createTabMenu();
-} catch (err) {
-	TAB_CONTEXT = false;
-}
-
-
-function createTabMenu() {
-	// create context menu on tab bar
-	return contextMenus.create({
-		title: "Popup this tab",
-		contexts: ["tab"],
-		onclick(info, tab) {
-			createPopup(tab);
+	browser.windows.onFocusChanged.addListener(windowId => {
+		var w = windowList.get(windowId);
+		if (!w) return;
+		if (w.type === "normal") {
+			lastFocusedNormalWindow = w;
+		}
+		if (handleFocusChange) {
+			handleFocusChange(w);
 		}
 	});
-}
 
-function createPopup(tab) {
-	var windowInfo = {
-		parent: tab.windowId,
-		index: tab.index
+	browser.windows.onRemoved.addListener(removeWindow);
+	browser.windows.onCreated.addListener(addWindow);
+		
+	function addWindow(w) {
+		windowList.set(w.id, w);
+		if (w.focused && w.type === "normal") {
+			lastFocusedNormalWindow = w;
+		}
+	}
+	
+	function removeWindow(windowId) {
+		windowList.delete(windowId);
+		if (lastFocusedNormalWindow.id === windowId) {
+			lastFocusedNormalWindow = null;
+		}
+	}
+	
+	function onFocusChange(cb) {
+		handleFocusChange = cb;
+	}
+	
+	function getLastFocusedNormalWindow() {
+		return lastFocusedNormalWindow || Array.from(windowList.values()).find(w => w.type === "normal");
+	}
+	
+	return {
+		onFocusChange,
+		getLastFocusedNormalWindow,
+		has: k => windowList.has(k),
+		ready: () => initialize
 	};
-	// popup tab
-	windows.create({
-		tabId: tab.id,
-		type: "popup"
-	}).then(window => {
-		addWindowInfo(window);
-		Object.assign(windowList.get(window.id), windowInfo);
-	});
-}
+})();
 
-// handle "Merge popup" ccontext menu, should only show if it is a popup window
-windows.onFocusChanged.addListener(windowId => {
-	var window = windowList.get(windowId);
-	if (!window) return;
+const popups = (function () {
+	const popups = new Map;
 	
-	if (window.type == "popup" && MERGE_CONTEXT == null) {
-		MERGE_CONTEXT = createMergeContext();
-	} else if (window.type != "popup" && MERGE_CONTEXT != null) {
-		contextMenus.remove(MERGE_CONTEXT);
-		MERGE_CONTEXT = null;
+	browser.windows.onRemoved.addListener(windowId => {
+		popups.delete(windowId);
+	});
+	
+	function _create(opt, setup) {
+		browser.windows.create(Object.assign({
+			type: "popup",
+			state: "maximized"
+		}, opt)).then(w => {
+			popups.set(w.id, w);
+			setup(w);
+		});
 	}
 	
-	if (!TAB_CONTEXT) {
-		if (window.type == "normal" && POPUP_CONTEXT == null) {
-			POPUP_CONTEXT = createPopupContext();
-		} else if (window.type != "normal" && POPUP_CONTEXT != null) {
-			contextMenus.remove(POPUP_CONTEXT);
-			POPUP_CONTEXT = null;
+	function create(tab) {
+		// popup tab
+		_create({tabId: tab.id}, w => {
+			w.parent = tab.windowId;
+			w.index = tab.index;
+		});
+	}
+	
+	function createFromURL(parentTab, url) {
+		_create({url}, w => {
+			const parentPopup = popups.get(parentTab.windowId);
+			if (parentPopup) {
+				w.parent = parentPopup.parent;
+				w.index = parentPopup.index;
+			} else {
+				w.parent = parentTab.windowId;
+				w.index = parentTab.index + 1;
+			}
+		});
+	}
+	
+	function merge(tab) {
+		let parent;
+		let index;
+		
+		const w = popups.get(tab.windowId);
+		if (w && windows.has(w.parent)) {
+			parent = w.parent;
+			index = w.index;
+		} else {
+			parent = windows.getLastFocusedNormalWindow().id;
+			index = -1;
 		}
+		
+		if (!parent) {
+			return toNormalWindow(tab);
+		}
+		
+		browser.windows.create({
+			tabId: tab.id,
+			left: 9999
+		}).then(() => {
+			// move tab to window
+			browser.tabs.move(tab.id, {
+				windowId: parent,
+				index
+			});
+			// FIXME: the temp window is automatically closed in Firefox 54, do we have to close it manually?
+		});
 	}
 	
-	window.lastFocus = performance.now();
+	function toNormalWindow(tab) {
+		browser.windows.create({tabId: tab.id});
+	}
+	
+	return {create, merge, toNormalWindow, createFromURL};
+})();
+
+browser.contextMenus.create({
+	title: "Open Link in Popup",
+	contexts: ["link"],
+	onclick(info, tab) {
+		popups.createFromURL(tab, info.linkUrl);
+	}
 });
 
+const TAB_MENU = tryCreateTabMenu();
+
+function dynamicMenus(createMenus) {
+	const menus = [];
+	
+	function create() {
+		if (!menus.length) {
+			for (const id of createMenus()) {
+				menus.push(id);
+			}
+		}
+	}
+	
+	function destroy() {
+		menus.forEach(id => browser.contextMenus.remove(id));
+		menus.length = 0;
+	}
+	
+	return {create, destroy};
+}
+
+const normalMenus = dynamicMenus(function *() {
+	if (!TAB_MENU) {
+		yield createPopupMenu();
+	}
+});
+
+const popupMenus = dynamicMenus(function *() {
+	yield createMergeMenu();
+	yield createToNormalMenu();
+});
+
+windows.onFocusChange(updateContextMenu);
+windows.ready(updateContextMenu);
+
+function updateContextMenu(w) {
+	if (!w) return;
+	if (w.type === "normal") {
+		popupMenus.destroy();
+		normalMenus.create();
+	} else if (w.type === "popup") {
+		popupMenus.create();
+		normalMenus.destroy();
+	}
+}
+
+// create context menu on tab bar
+// tab context menu only works on Firefox 53+
+function tryCreateTabMenu() {
+	try {
+		return createPopupMenu(["tab"]);
+	} catch (err) {
+		// do nothing
+	}
+}
+
 // create "Popup this tab" context menu
-function createPopupContext() {
-	return contextMenus.create({
-		title: "Popup this tab",
+function createPopupMenu(contexts = ["page"]) {
+	return browser.contextMenus.create({
+		title: "Popup This Tab",
+		contexts,
 		onclick(info, tab) {
-			createPopup(tab);
+			popups.create(tab);
 		}
 	});
 }
 
 // create "Merge popup" context menu
-function createMergeContext() {
-	return contextMenus.create({
-		title: "Merge popup",
+function createMergeMenu() {
+	return browser.contextMenus.create({
+		title: "Merge Popup",
+		contexts: ["page"],
 		onclick(info, tab) {
-			mergePopup(tab.id, tab.windowId);
+			popups.merge(tab);
 		}
 	});
 }
 
-// merge popup back to normal window
-function mergePopup(tabId, windowId) {
-	var window = windowList.get(windowId);
-	
-	if (window.type != "popup") {
-		return;
-	}
-	
-	Promise.resolve()
-		.then(() => {
-			// use parent id
-			if (window.parent != null && windowList.has(window.parent)) {
-				return window;
-			}
-			// use last focused
-			var found;
-			for (var w of windowList.values()) {
-				if (w.type == "normal" && (!found || w.lastFocus > found.lastFocus)) {
-					found = w;
-				}
-			}
-			if (found) {
-				return {
-					parent: found.id,
-					index: -1
-				};
-			}
-		})
-		// merge popup
-		.then(target => {
-			// create a temp window, can't move popup back to normal window directly
-			return windows.create({
-				tabId: tabId,
-				left: 9999
-			}).then(() => {
-				var pendings = [];
-				pendings.push(windows.remove(window.id));
-				if (target) {
-					// move tab to window
-					pendings.push(tabs.move(tabId, {
-						windowId: target.parent,
-						index: target.index
-					}));
-				} else {
-					// create a new window
-					pendings.push(windows.create({
-						tabId: tabId
-					}));
-				}
-				// FIXME: the temp window is automatically closed in Firefox 54, do we have to close it manually?
-				return Promise.all(pendings);
-			}).catch(console.error);
-		});
-}
-
-windows.onRemoved.addListener(windowId => {
-	windowList.delete(windowId);
-});
-
-windows.onCreated.addListener(addWindowInfo);
-
-windows.getAll({
-	windowTypes: ["normal", "popup"]
-}).then(windowInfos => {
-	for (var windowInfo of windowInfos) {
-		addWindowInfo(windowInfo);
-	}
-});
-
-function addWindowInfo(window) {
-	if (windowList.has(window.id)) return;
-	windowList.set(window.id, {
-		id: window.id,
-		type: window.type,
-		parent: null,
-		lastFocus: 0,
-		index: -1
+function createToNormalMenu() {
+	return browser.contextMenus.create({
+		title: "Convert Popup into Normal Window",
+		contexts: ["page"],
+		onclick(info, tab) {
+			popups.toNormalWindow(tab);
+		}
 	});
 }
